5b0546cfe702f9323adad043ea6db7dc9a1b0764
mauricio10am@gmail.com <mauricio10am@gmail.com>
2018-05-15 23:32:41 -0300

    utilizando o conceito de orientação a objetos, implementando o objeto cachorro e o metodo latir

---
+++ src/treino_java_004/TREINO_JAVA_004.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package treino_java_004;
+
+/**
+ *
+ * @author EngMauricio
+ */
+public class TREINO_JAVA_004 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        cachorro mycachorro = new cachorro();
+    
+        mycachorro.Latir();
+        mycachorro.Andar();
+        mycachorro.Pular();
+    }
+    
+}
---
+++ src/treino_java_004/cachorro.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package treino_java_004;
+
+/**
+ *
+ * @author EngMauricio
+ */
+public class cachorro{
+    
+    public void Latir(){
+        
+        System.out.println(" cachorro latindo ");
+    }
+    public void Andar(){
+        
+        System.out.println(" cachorro andando ");  
+    }
+    public void Pular(){
+     
+        System.out.println(" cachorro pulando ");
+    }
+    }
+    
+
